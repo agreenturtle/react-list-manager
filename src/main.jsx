2ebf5e35de76158ab28cf43b1379f565baf83dc1
James Chen <james@steelhouse.com>
2016-04-25 16:07:24 -0700

    List Manager App

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
  var React = require('react');
  var ReactDOM = require('react-dom');
- var List = require('./components/List.jsx');
+ var ListManager = require('./components/ListManager.jsx');
 
- ReactDOM.render(<List />, document.getElementById('app'));
+ ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('app'));
